862adc874e69b9b1aa973f7d5f429a36bf9b0b3f
Paul van Schayck <polleke@gmail.com>
2014-04-07 13:28:45 +0200

    Initial work on converting to Yii2

--- views/layouts/webshell.php
+++ views/layouts/webshell.php
@@ -1,10 +1,18 @@
+<?php
+use yii\helpers\Html;
+?>
+<?php $this->beginPage() ?>
 <!doctype html>
 <html>
 <head>
-    <meta charset="<?php echo Yii::app()->charset?>">
-    <title><?php echo $this->pageTitle?></title>
+    <meta charset="<?php echo Yii::$app->charset?>">
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
 </head>
 <body>
+<?php $this->beginBody() ?>
 <?php echo $content?>
+<?php $this->endBody() ?>
 </body>
 </html>
+<?php $this->endPage() ?>
